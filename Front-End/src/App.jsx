4ab56c768daa3b5b74668fd91951796db959d6a3
Phenrique1628 <henriquepedro02964@gmail.com>
2025-09-25 07:40:06 -0400

    test

--- Front-End/src/App.jsx
+++ Front-End/src/App.jsx
@@ -5,16 +5,18 @@ function App() {
   const [email, setEmail] = useState("");
   const [senha, setSenha] = useState("");
 
-  const teste = () => {
-    console.log("abra cadabraa")
+
+  const handlesubmit = (e) => {
+    e.preventDefault()
+    console.log("handlesubmit")
   }
 
   return (
     <>
-     <form className='form'>
+     <form onSubmit={(e) => handlesubmit(e)} className='form'>
     <h1>Cadastro de Reservas</h1>
       <h2>Email</h2>
-      <input type="email" value={email} onChange={() => setEmail(value)}/>
+      <input type="email" value={email} onChange={() => setEmail()}/>
       <h2>Senha</h2>
       <input type="password" value={senha} onChange={() => setSenha()}/>
       <button type='submit'/>
